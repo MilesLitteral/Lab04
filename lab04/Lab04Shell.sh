4da0a775a8ddb74a3773bcd4b14534d46d3c73d9
MilesLitteral <Mandaloe2@gmail.com>
2016-05-18 11:54:02 -0400

    Lab04

---
+++ lab04/Lab04Shell.sh
@@ -0,0 +1,6 @@
+#Prints Hello World and makes a .txt
+
+echo "Hello World!"
+mkdir "UNDERACHIVERS"
+cd UNDERACHIVERS
+touch "TRACKLIST.txt"
---
+++ lab04/README.md
@@ -0,0 +1,1 @@
+Welcome to Lab 04, it's about Subversion Control and Shell Scripting
